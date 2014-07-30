dfee19fbce677c4d5207170798b8e6d892926d02
619619 <619619.psp@gmail.com>
2014-07-30 14:45:15 +0200

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Jul 24 01:05:19 CEST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "menny-VirtualBox"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
